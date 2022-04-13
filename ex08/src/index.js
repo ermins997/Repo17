5937ee15c12ca7cdb1a9231a3956511a7ded3f3f
Ermin Sacic <sacic_ermin97@hotmail.com>
2022-04-14 00:58:55 +0200

    small changes

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,27 +1,35 @@
 var users = [
-    { firstName: "John", lastName: "Doe", age: 24, gender: "male " },
-    { firstName: "Jane", lastName: "Doe", age: 5, gender: "female " },
-    { firstName: "Jim", lastName: "Carrey", age: 54, gender: "male " },
-    { firstName: "Kate", lastName: "Winslet", age: 40, gender: "female " },
-  ];
-  function getUsers() {
-    var output = "";
-    for (let i = 0; i < users.length; i++) {
-      output += `${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender}`;
-    }
-    console.log(output);
-    return output;
+  { firstName: "John", lastName: "Doe", age: 24, gender: "male " },
+  { firstName: "Jane", lastName: "Doe", age: 5, gender: "female " },
+  { firstName: "Jim", lastName: "Carrey", age: 54, gender: "male " },
+  { firstName: "Kate", lastName: "Winslet", age: 40, gender: "female " }
+];
+
+function getUsers() 
+{
+  var output="";
+  for (let i = 0; i < users.length; i++) 
+  {
+   output+=`${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender} \n`;
+     
+
   }
-  
-  function findUsers(lastName, gender) {
-    try {
-      let users = _.find(users, { lastName: lastName, gender: gender });
-      let iFindUser = `${users.firstName} ${users.lastName} is ${users[i].age}, ${users.gender}`;
+  return output;
+   
+}
+function findUsers(lastName,gender) 
+{
+  try {
+      let users=_find(users,{lastName:lastName,gender:gender});
+      let iFindUser=`${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender} \n`;
       return iFindUser;
-    } catch (error) {
+      
+  } catch (error) {
       console.log("Cannot read property 'firstName' of undefined");
       return "Cannot read property 'firstName' of undefined";
-    }
+      
   }
-  console.log(getUsers());
-  console.log(findUsers());
+  
+}
+console.log(getUsers());
+console.log(findUsers());
